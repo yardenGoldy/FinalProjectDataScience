f838ffbe526e22a2137402d86595a556308d47c0
Unknown <yarden.goldy@gmail.com>
2018-12-22 16:00:16 +0200

    Add days without date

--- AddFields.py
+++ AddFields.py
@@ -3,7 +3,7 @@ df = pandas.read_csv('hotels_data.csv')
 df.head()
 df["Snapshot Date"] = pandas.to_datetime(df["Snapshot Date"])
 df["Checkin Date"] = pandas.to_datetime(df["Checkin Date"])
-df["DayDiff"] = df["Checkin Date"] - df["Snapshot Date"]
+df["DayDiff"] = (df["Checkin Date"] - df["Snapshot Date"]).dt.days
 df["WeekDay"] = df["Checkin Date"].dt.day_name()
 df["DiscountDiff"] = df["Original Price"] - df["Discount Price"]
 df["DiscountPerc"] = (df["DiscountDiff"] / df["Original Price"]) * 100

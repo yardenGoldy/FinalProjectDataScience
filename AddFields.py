7ae97a7dd276b813ac9b88e14b59d95bd73050fa
Unknown <yarden.goldy@gmail.com>
2018-12-22 16:16:49 +0200

    Finish add fields task

--- AddFields.py
+++ AddFields.py
@@ -1,10 +1,8 @@
 import pandas
 df = pandas.read_csv('hotels_data.csv')
 df.head()
-df["Snapshot Date"] = pandas.to_datetime(df["Snapshot Date"])
-df["Checkin Date"] = pandas.to_datetime(df["Checkin Date"])
-df["DayDiff"] = (df["Checkin Date"] - df["Snapshot Date"]).dt.days
-df["WeekDay"] = df["Checkin Date"].dt.day_name()
+df["DayDiff"] = (pandas.to_datetime(df["Checkin Date"]) - pandas.to_datetime(df["Snapshot Date"])).dt.days
+df["WeekDay"] = (pandas.to_datetime(df["Checkin Date"])).dt.day_name()
 df["DiscountDiff"] = df["Original Price"] - df["Discount Price"]
 df["DiscountPerc"] = (df["DiscountDiff"] / df["Original Price"]) * 100
 df.head()
